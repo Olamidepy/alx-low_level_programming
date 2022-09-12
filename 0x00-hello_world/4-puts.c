9a36b92115b7096ca3ea9d50d08ae9ff10894c1f
olamidepy <ooluwadamilare346@gmail.com>
2022-09-13 00:24:54 +0100

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *program should print "programming is like a multilingua puzzle
+ *
+ * use the function puts
+ * program should end with the value zero
+ */
+int main(void)
+{
+  char Mystring[] = "programming is like a multilingua puzzle";
+{
+puts("Mystring");
+}
+return (0);
+}
