a32af124bd70dac81e5cbd060fb4dbe9f8712b39
olamidepy <ooluwadamilare346@gmail.com>
2022-09-16 22:56:06 +0100

    supper.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
-
-void positive_or_negative(int i);
-int largest_number(int, int, int);
-void print_remaining_days(int, int, int);
-int convert_day(int, int);
-
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 #endif /* MAIN_H */
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 /**
 *main - header file for protoypes
 *void protoypes (void)
 *int protoypes (int)
 */
-
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -19,5 +17,4 @@ int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
-
 #endif /* MAIN_H */
