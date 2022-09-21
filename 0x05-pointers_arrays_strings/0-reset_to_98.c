521b07e6e9ffb81670f076766fd64c1cf8ddaa2b
olamidepy <ooluwadamilare346@gmail.com>
2022-09-21 23:08:02 +0100

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,22 +1,10 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * take a pointer to an int
  * update the value to 98
  * always return 0
  */
-int main(void)
-{
 void reset_to_98(int *n);
-*n = 402;
-{
-putchar("*n");
-putchar("\n");
 }
-reset_to_98(&n);
-{
-putchar("*n");
-putchar("\n");
+*n = 98;
 }
- return 0;
-}
-     
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*main - header file for protoypes
+*void protoypes-void- reset_to_9(int *n)
+*int protoypes (int)
+*/
+int_putchar(char c)
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+#endif /* MAIN_H */
