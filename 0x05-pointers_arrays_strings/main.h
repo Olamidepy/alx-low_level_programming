20ac8b986bd6cb8a660f5232ed11e816ac9fae5f
olamidepy <ooluwadamilare346@gmail.com>
2022-09-23 00:27:53 +0100

    reset to 98

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,13 +1,13 @@
+
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
 /**
-*main - header file for protoypes
-*void protoypes-void- reset_to_9(int *n)
-*int protoypes (int)
-*/
-
-int _putchar(char c)
+ *void prototypes()
+ *int prototypes()
+ *char prototypes()
+ */
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -18,5 +18,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-#endif /* MAIN_H */
+#endif /*MAIN_H*/
