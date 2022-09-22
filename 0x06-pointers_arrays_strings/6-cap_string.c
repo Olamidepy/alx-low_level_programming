e66656fa40b19b9a2dbdb4c1511bd4d5d2ecf0ee
olamidepy <ooluwadamilare346@gmail.com>
2022-09-22 19:43:26 +0100

    6-concate

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,31 @@
 #include "main.h"
 
 /**
- * leet - encodes a string into 1337
- * @s: string to encode
+ * cap_string - capitalizes all words in a string
+ * @s: string to capitalize
  *
  * Return: address of s
  */
-char *leet(char *s)
+
+char *cap_string(char *s)
 {
-int i, j;
-char a[] = "aAeEoOtTlL";
-char b[] = "4433007711";
-for (i = 0; *(s + i); i++)
+int i = 0, j;
+char a[] = " \t\n,;.!?\"(){}";
+while (*(s + i))
 {
-for (j = 0; j <= 9; j++)
+if (*(s + i) >= 'a' && *(s + i) <= 'z')
 {
-if (a[j] == *(s + i))
-*(s + i) = b[j];
+if (i == 0)
+*(s + i) -= 'a' - 'A';
+else
+{
+for (j = 0; j <= 12; j++)
+if (a[j] == *(s + i - 1))
+*(s + i) -= 'a' - 'A';
 }
+}
+}
+i++;
 }
 return (s);
 }
