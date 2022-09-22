26e63d033a795bbfad5fd5d3f63188795024de79
olamidepy <ooluwadamilare346@gmail.com>
2022-09-22 20:10:09 +0100

    7-concate

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,11 +12,11 @@ char *leet(char *s)
 int i, j;
 char a[] = "aAeEoOtTlL";
 char b[] = "4433007711";
- for (i = 0; *(s + i); i++);
+for (i = 0; *(s + i); i++)
 {
-  for (j = 0; j <= 9; j++);
+for (j = 0; j <= 9; j++)
 {
-  if (a[j] == *(s + i));
+if (a[j] == *(s + i))
 *(s + i) = b[j];
 }
 }
