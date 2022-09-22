7e4aa98b7dad471f67b608b81b6a9ef2cf239ba8
olamidepy <ooluwadamilare346@gmail.com>
2022-09-22 19:11:48 +0100

    3-concate

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
-* _strcmp - Function which compare two strings and
-* @s1: first string
-* @s2:second string
-* Return:
-* returns zero if s1 == s2
-* returns negative number if s1 < s2
-* returns positive number if s1 > s2
+*  _strcmp - Function which compare two strings and
+*@s1: first string
+*@s2:second string
+*Return:
+*returns zero if s1 == s2
+*returns negative number if s1 < s2
+*returns positive number if s1 > s2
 */
+
 int _strcmp(char *s1, char *s2)
 {
 int i = 0, diff = 0;
@@ -33,5 +35,5 @@ break;
 else
 i++;
 }
- return (diff);
+return (diff);
 }
